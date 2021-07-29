674b282a7390864068c8cea58a5356cda0e80b82
abhisheksanchula99 <abhisheksanchula@gmail.com>
2021-07-29 21:26:53 +0530

    Js files are committed after changes

---
+++ qi.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Check date</h2>
+
+<button type="checkbox"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
